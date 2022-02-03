deb0adc0ad12c310e7927b90696af36c3544fd32
Sally Heo <eunhasu9@gmail.com>
2022-02-03 14:34:30 -0800

    commit week_2

---
+++ Week_2/Scripts/week2class.R
@@ -0,0 +1,17 @@
+## This is my first script. I am learning how to import data
+## created by: Sally Heo
+## created on: 2022-02-03
+#########################################################
+
+### load libraries ###
+library(tidyverse)
+library(here)
+
+
+### Read in data #####
+weightData <- read_csv(here("Week_2","Data","weightdata.csv"))
+
+
+
+
+
