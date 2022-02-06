f74789aba6086336243c73d556e872ec7f243685
sallyheo <98852800+sallyheo@users.noreply.github.com>
2022-02-05 19:07:49 -0800

    Update week2class.R

--- Week_2/Scripts/week2class.R
+++ Week_2/Scripts/week2class.R
@@ -4,14 +4,17 @@
 #########################################################
 
 ### load libraries ###
-library(tidyverse)
-library(here)
+library(tidyverse) #Do this every time and be sure you are in the line and hit run
+library(here) #Do this everyt time and be sure you are in the line and hit run
 
 
 ### Read in data #####
-weightData <- read_csv(here("Week_2","Data","weightdata.csv"))
+weightData <- read_csv(here("Week_2","Data","weightdata.csv")) #weight data
 
-
+# Data analysis #####
+head(weightData) # Look at the top 6 lines of the dataframe
+tail(weightData) # Look at the bottom 6 lines of the dataframe
+View(weightData) # Shows the whole data set, and this opens a new window
 
 
 
